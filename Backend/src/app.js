0100e7924bfc31a21e8aa6755398ee2cff4ebe42
SUMIT MADDESHIYA <sumitmaddeshiya99@gmail.com>
2026-05-27 19:24:52 +0530

    Updated production CORS and cookie config

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,22 +1,26 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
-const cors = require("cors")
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.set("trust proxy", 1)
+
+app.set("trust proxy", 1);
+
 app.use(cors({
     origin: [
         "http://localhost:5173",
         "https://bank-transaction-system-nu.vercel.app"
     ],
-    credentials: true
-}))
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
 const authRoutes = require("./routes/auth.routes");
-app.use("/api/auth", authRoutes);
 
+app.use("/api/auth", authRoutes);
 
 module.exports = app;
