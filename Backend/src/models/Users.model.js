6e3a63de80914791dc631e9f40db101e210c2d96
SUMIT MADDESHIYA <sumitmaddeshiya99@gmail.com>
2026-05-28 22:47:01 +0530

    updated user schems

--- Backend/src/models/Users.model.js
+++ Backend/src/models/Users.model.js
@@ -3,16 +3,24 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
-        required: true
+        required: [true, "Username is required"]
     },
     email: {
         type: String,
-        required:true
+        required:[true, "Email is required"],
+        trim: true,
+        lowercase: true,
+        match: [ /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, "Please enter a valid email" ],
+        unique: [true, "Email already exists"]
     },
     password: {
         type: String,
-        required: true
+        required: [true, "Password is required"],
+        minlength: [6, "Password must be at least 6 characters long"],
+        // select: false
     }
+},{
+    timestamps: true
 })
 
 module.exports = mongoose.model("Users", userSchema);
